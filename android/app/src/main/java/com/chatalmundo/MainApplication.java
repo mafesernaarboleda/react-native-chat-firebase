8201aa15d24c54d5556869d877f0ea959c192f92
Maria Serna <mafesernaarboleda@gmail.com>
2018-02-24 12:50:17 -0500

    resolve error android

--- android/app/src/main/java/com/chatalmundo/MainApplication.java
+++ android/app/src/main/java/com/chatalmundo/MainApplication.java
@@ -9,11 +9,20 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -26,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new FBSDKPackage()
+              new FBSDKPackage(mCallbackManager)
       );
     }
 
@@ -45,5 +54,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    AppEventsLogger.activateApp(this);
   }
 }
