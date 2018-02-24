dc2d4088532e56d9a433a740cdd81e753c836ff1
Maria Serna <mafesernaarboleda@gmail.com>
2018-02-24 10:12:56 -0500

    firebase example

---
+++ app/config/firebase.example.js
@@ -0,0 +1,9 @@
+/**
+ * @author Maria Fernanda Serna
+ */
+
+export const firebaseConfig = {
+    apiKey: "apikey",
+    authDomain: "name.firebaseapp.com",
+    databaseURL: "https://name.firebaseio.com/"
+}
