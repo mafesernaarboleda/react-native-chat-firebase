e0769c86a08ace2a489d9d4c28d59d68a0f3f6cd
Maria Fernanda Serna Arboleda <mafesernaarboleda@gmail.com>
2018-02-24 10:13:42 -0500

    Delete firebase.js

--- app/config/firebase.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Maria Fernanda Serna
- */
-
-export const firebaseConfig = {
-apiKey: "AIzaSyA5O7l0iJw8BISTzVvUNtAhTVnY8aUaQsI",
-authDomain: "reactdebo.firebaseapp.com",
-databaseURL: "https://reactdebo.firebaseio.com/"
-}
